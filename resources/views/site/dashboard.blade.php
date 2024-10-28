51afd71d3e21117bdb85eb610d72480a712c9f62
Paulo Rodrigues <paulinhojrodrigues@gmail.com>
2024-10-28 09:13:08 -0400

    claus

--- resources/views/site/dashboard.blade.php
+++ resources/views/site/dashboard.blade.php
@@ -70,11 +70,12 @@
                                 @endif
 
                                 <h5 class="mt-2">- Sorteio: {{ $campanha->sorteio->nome }}</h5>
-                                <strong class="mt-1">- Data: {{ dateTimeUsParaDateTimeBr($campanha->data) }}</strong>
+                                <strong class="mt-1">- Data: {{ dateTimeUsParaBr($campanha->data) }}</strong>
 
                                 <h5 class="mt-2">- Descrição</h5>
 
                                 {!! nl2br(e($campanha->descricao)) !!}
+
 
                             </div>
                         </div>
